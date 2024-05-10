51c5dbd7a1715e13aaabacfdf8c6ae34c59dde8f
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2024-05-10 21:52:24 +0600

    added card label

--- Set/SetCardView.swift
+++ Set/SetCardView.swift
@@ -11,9 +11,14 @@ import UIKit
 class SetCardView: UIView {
     override func draw(_ rect: CGRect) {
         let card = UIBezierPath(roundedRect: bounds, cornerRadius: 5.0)
-        UIColor.systemGray3.setFill()
+        UIColor.systemGray5.setFill()
         UIColor.black.setStroke()
         card.fill()
+        
+        let font = UIFontMetrics(forTextStyle: .body).scaledFont(for: UIFont.preferredFont(forTextStyle: .body).withSize(26.0))
+        let cardLabel = NSAttributedString(string: "■", attributes: [.font: font, .foregroundColor: UIColor.red])
+        let labelRect = bounds.insetBy(dx: bounds.width / 2 - cardLabel.size().width / 2, dy: bounds.height / 2 - cardLabel.size().height / 2)
+        cardLabel.draw(in: labelRect)
     }
 
 }
