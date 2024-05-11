6a9965f82f8abf79b430122b69951c509cb814b3
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2024-05-11 15:12:03 +0600

    card visibility handled with tap

--- Set/SetCardView.swift
+++ Set/SetCardView.swift
@@ -9,18 +9,32 @@ import UIKit
 
 @IBDesignable
 class SetCardView: UIView {
+    private var show = true
+    var text = "■"
+    var textColor = UIColor.orange
+    var fillColor = UIColor.systemGray5
+    
+    func setCard(withText text: String, withColor color: UIColor, withBackground bgColor: UIColor) {
+        self.text = text
+        self.textColor = color
+        self.fillColor = bgColor
+    }
+    
     override func draw(_ rect: CGRect) {
-        let card = UIBezierPath(roundedRect: bounds, cornerRadius: 5.0)
-        UIColor.black.setStroke()
-        card.stroke()
-        
-        let font = UIFontMetrics(forTextStyle: .body).scaledFont(for: UIFont.preferredFont(forTextStyle: .body).withSize(26.0))
-        let cardLabel = NSAttributedString(string: "■", attributes: [.font: font, .foregroundColor: UIColor.red])
-        let labelRect = bounds.insetBy(dx: bounds.width / 2 - cardLabel.size().width / 2, dy: bounds.height / 2 - cardLabel.size().height / 2)
-        cardLabel.draw(in: labelRect)
+        if show {
+            let card = UIBezierPath(roundedRect: bounds, cornerRadius: 7.0)
+            fillColor.setFill()
+            card.fill()
+            
+            let font = UIFontMetrics(forTextStyle: .body).scaledFont(for: UIFont.preferredFont(forTextStyle: .body).withSize(26.0))
+            let cardLabel = NSAttributedString(string: text, attributes: [.font: font, .foregroundColor: textColor])
+            let labelRect = bounds.insetBy(dx: bounds.width / 2 - cardLabel.size().width / 2, dy: bounds.height / 2 - cardLabel.size().height / 2)
+            cardLabel.draw(in: labelRect)
+        }
     }
 
     @objc func cardTapped(gesture: UITapGestureRecognizer) {
-        self.backgroundColor = UIColor.green
+        show = false
+        setNeedsDisplay()
     }
 }
