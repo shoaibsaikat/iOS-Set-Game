0ae5e71811c475de74e27505fa3999aa5cd59b1a
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2024-08-05 10:16:31 +0600

    still some issue persisted, fixed by setting height and weight for specific orientation

--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -30,10 +30,10 @@ class ViewController: UIViewController, SetCardParent {
     @IBOutlet var cards: [SetCardView]! {
         didSet {
             for card in cards {
+                // hide all cards by default
                 card.backgroundColor = UIColor(white: 0, alpha: 0)
                 let tap = UITapGestureRecognizer(target: card, action: #selector(card.cardTapped(_:)))
                 card.addGestureRecognizer(tap)
-                
             }
         }
     }
